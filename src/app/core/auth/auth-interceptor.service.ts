46c8ca6044ded2da165173f17ea1643793968fb8
nahuel <ncastrocaceres@safareyes.es>
2024-11-13 20:33:37 +0100

    implementacion de interceptor y configuración del auth-service.service.spec.ts

---
+++ src/app/core/auth/auth-interceptor.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService} from '../auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Obtén el token del AuthService
+    const token = this.authService.getToken();
+
+    // Clona la solicitud y añade el encabezado Authorization con el token
+    if (token) {
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+
+    // Si no hay token, envía la solicitud original
+    return next.handle(req);
+  }
+}
