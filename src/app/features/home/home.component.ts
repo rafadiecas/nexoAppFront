cb37dca57f83fa0a050c55386afe0af717809ccc
rafdiecas <rdiezcastro@safareyes.es>
2024-11-13 20:40:44 +0100

    add - componente input fotos, falta estilo

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {DesaparicionPrincipalComponent} from './componentes/desaparicion-principal/desaparicion-principal.component';
+import {InputFotosComponent} from '../perfil-usuario/componentes/input-fotos/input-fotos.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    DesaparicionPrincipalComponent
+    DesaparicionPrincipalComponent,
+    InputFotosComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
