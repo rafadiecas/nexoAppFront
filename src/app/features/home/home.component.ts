5fcdc658010f86d430ec59b266db4bb13404952a
rafdiecas <rdiezcastro@safareyes.es>
2024-11-15 14:58:09 +0100

    add - componente edita usuario, falta verificar

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {DesaparicionPrincipalComponent} from './componentes/desaparicion-principal/desaparicion-principal.component';
+import {EditaUsuarioComponent} from '../perfil-usuario/componentes/edita-usuario/edita-usuario.component';
+import {InputFotosComponent} from '../perfil-usuario/componentes/input-fotos/input-fotos.component';
+import {MapaPrincipalComponent} from './componentes/mapa-principal/mapa-principal.component';
 
 
 @Component({
---
+++ src/app/features/perfil-usuario/componentes/edita-usuario/edita-usuario.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CivilService} from '../../../../servicios/civil.service';
+
+@Component({
+  selector: 'app-edita-usuario',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './edita-usuario.component.html',
+  styleUrl: './edita-usuario.component.css'
+})
+export class EditaUsuarioComponent implements OnInit{
+  civilForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private civilService:CivilService) {
+    this.civilForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['',Validators.required],
+      telefono: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
+      dni: ['', [Validators.required, Validators.pattern(/^\d{8}[A-Z]$/)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.civilService.getCivil(3).subscribe(civil => {
+      this.civilForm.patchValue(civil);
+      this.civilForm.get('dni')?.disable();
+    });
+  }
+
+  submit(): void {
+    if (this.civilForm.valid) {
+
+      this.civilForm.get('dni')?.enable();
+
+      this.civilService.actualizarCivil(3, this.civilForm.value).subscribe({
+        next: (response) => {
+          console.log(response.mensaje);
+
+          this.civilService.getCivil(3).subscribe(civil => {
+            this.civilForm.patchValue(civil);
+
+            this.civilForm.get('dni')?.disable();
+          });
+        },
+        error: (error) => {
+          console.error('Error al actualizar el civil:', error);
+
+          this.civilForm.get('dni')?.disable();
+        },
+      });
+    }
+  }
+
+
+}
