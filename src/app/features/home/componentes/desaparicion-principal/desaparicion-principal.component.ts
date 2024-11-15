0832d716ff37ffad9b685a8cef22cdccc7ca9afe
daw <avelazquezmunoz@safareyes.es>
2024-11-15 16:43:34 +0100

    desaparicion principal

--- src/app/features/home/componentes/desaparicion-principal/desaparicion-principal.component.ts
+++ src/app/features/home/componentes/desaparicion-principal/desaparicion-principal.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DesaparicionService} from '../../../../servicios/desaparicion.service';
-import {MatGridList, MatGridTile, MatGridTileText} from '@angular/material/grid-list';
-import {NgForOf} from '@angular/common';
-import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import {DatePipe, NgForOf, UpperCasePipe} from '@angular/common';
+import {MatCard, MatCardContent, MatCardImage} from '@angular/material/card';
 
 @Component({
   selector: 'app-desaparicion-principal',
@@ -11,9 +11,11 @@ import {MatCard, MatCardContent} from '@angular/material/card';
     MatGridList,
     MatGridTile,
     NgForOf,
-    MatGridTileText,
     MatCardContent,
-    MatCard
+    MatCard,
+    UpperCasePipe,
+    DatePipe,
+    MatCardImage,
   ],
   templateUrl: './desaparicion-principal.component.html',
   styleUrl: './desaparicion-principal.component.css'
@@ -27,8 +29,6 @@ export class DesaparicionPrincipalComponent implements OnInit {
   ngOnInit(): void {
     this.desaparicionPrincipalService.getDesaparicionesPrincipal().subscribe((data) =>{
       this.desapariciones = data;
-      console.log(data);
-      console.log(this.desapariciones)
     });
   }
 
