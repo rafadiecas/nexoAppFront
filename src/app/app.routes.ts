5973fd73753113610424e3f47f40283cc981dbf5
rafdiecas <rdiezcastro@safareyes.es>
2024-11-15 19:49:58 +0100

    perfil usuario ya es responsive menos texto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
+import {PerfilCivilComponent} from './features/perfil-usuario/componentes/perfil-civil/perfil-civil.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  {path: 'usuario/civil', component:PerfilCivilComponent}
 ];
--- src/app/features/perfil-usuario/componentes/parte-izquierda-civil/parte-izquierda-civil.component.ts
+++ src/app/features/perfil-usuario/componentes/parte-izquierda-civil/parte-izquierda-civil.component.ts
@@ -1,26 +1,23 @@
-import {Component, Output, EventEmitter, OnInit} from '@angular/core';
-import {NgForOf} from '@angular/common';
-import {UsuarioMenu} from '../../../../modelos/UsuarioMenu';
-import {CivilService} from '../../../../servicios/civil.service';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { UsuarioMenu } from '../../../../modelos/UsuarioMenu';
+import { CivilService } from '../../../../servicios/civil.service';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-parte-izquierda-civil',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    MatIcon
   ],
   templateUrl: './parte-izquierda-civil.component.html',
-  styleUrls: ['./parte-izquierda-civil.component.css'] // Cambiado a styleUrls
+  styleUrls: ['./parte-izquierda-civil.component.css']
 })
 export class ParteIzquierdaCivilComponent implements OnInit {
 
   menu: UsuarioMenu | undefined;
-
-  constructor(private civilService: CivilService) {}
-
-  ngOnInit() {
-    this.civilService.civilMenu(3).subscribe(menu => this.menu = menu);
-  }
+  selectedOption: string = '';
 
   menuOptions = [
     { label: 'Editar', value: 'editar' },
@@ -30,8 +27,23 @@ export class ParteIzquierdaCivilComponent implements OnInit {
 
   @Output() optionSelected = new EventEmitter<string>();
 
+  constructor(private civilService: CivilService) {}
+
+  ngOnInit() {
+    this.civilService.civilMenu(3).subscribe(menu => this.menu = menu);
+  }
+
   selectOption(option: string) {
+    this.selectedOption = option;
     this.optionSelected.emit(option);
   }
 
+  getIcon(value: string): string {
+    switch (value) {
+      case 'editar': return 'edit';
+      case 'desapariciones': return 'search';
+      case 'seguimiento': return 'track_changes';
+      default: return 'help_outline';
+    }
+  }
 }
