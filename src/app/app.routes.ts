0daa3891637fa6fc239aa12abbff446325c302a3
daw <avelazquezmunoz@safareyes.es>
2024-11-19 18:40:54 +0100

    pantalla admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
 import {PerfilCivilComponent} from './features/perfil-usuario/componentes/perfil-civil/perfil-civil.component';
 import {DesaparicionComponent} from './features/desaparicion/desaparicion.component';
-import {AdminPanelComponent} from './features/Admin/admin-panel/admin-panel.component';
+import {AdminPanelComponent} from './features/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
