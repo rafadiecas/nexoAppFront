5c38cafe30a365af88cfd76280daf4a2e7aeb6fc
Alex <amartinenghivelazquez@safareyes.es>
2024-11-20 21:09:26 +0100

    formulario crear-desaparicion creado

--- src/app/features/perfil-usuario/componentes/crear-desaparicion/crear-desaparicion.component.ts
+++ src/app/features/perfil-usuario/componentes/crear-desaparicion/crear-desaparicion.component.ts
@@ -7,6 +7,7 @@ import { Sexo } from '../../../../modelos/Sexo'; // Importamos el enum Sexo
 import { Complexion } from '../../../../modelos/Complexion';
 import {NgForOf} from '@angular/common';
 import {DesaparicionService} from '../../../../servicios/desaparicion.service'; // Importamos el enum Complexion
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-crear-desaparicion',
@@ -16,6 +17,7 @@ import {DesaparicionService} from '../../../../servicios/desaparicion.service'; 
     InputFotosComponent,
     LocalizacionComponent,
     NgForOf,
+    CommonModule
     // Agregamos LocalizacionComponent
   ],
   standalone: true
@@ -25,6 +27,8 @@ export class DesaparicionFormComponent {
   archivos: File[] = [];
   sexoOptions = Object.values(Sexo); // Obtenemos las opciones del enum Sexo
   complexionOptions = Object.values(Complexion); // Obtenemos las opciones del enum Complexion
+  currentStep = 0;
+  steps = ['Paso 1', 'Paso 2', 'Paso 3']
 
   constructor(private fb: FormBuilder, private desaparicionService: DesaparicionService) {
     this.desaparicionForm = this.fb.group({
@@ -42,21 +46,54 @@ export class DesaparicionFormComponent {
         descripcion: [''],
       }),
       lugarDTO: this.fb.group({
-        provincia: [''], // Se gestionará con LocalizacionComponent
-        localidad: [''], // Se gestionará con LocalizacionComponent
-        calle: [''],     // Se gestionará con LocalizacionComponent
+        provincia: [''],
+        localidad: [''],
+        calle: [''],
       }),
     });
+
   }
 
   onFilesChanged(filesData: FileData[]): void {
     this.archivos = filesData.map(fileData => fileData.file as File);
   }
 
-  // Método que recibe los cambios desde LocalizacionComponent
   onAddressChanged(address: any): void {
-    this.desaparicionForm.get('lugar')?.patchValue(address);
+    this.desaparicionForm.get('lugarDTO')?.patchValue(address);
   }
+
+  nextStep() {
+    const currentFormGroup = this.getCurrentStepGroup();
+    if (currentFormGroup && currentFormGroup.valid) {
+      this.currentStep++;
+    } else {
+      currentFormGroup?.markAllAsTouched();
+    }
+  }
+
+  prevStep(): void {
+    if (this.currentStep > 0) {
+      this.currentStep--;
+    }
+  }
+
+  goToStep(step: number): void {
+    const formValid = this.getCurrentStepGroup()?.valid ?? true;
+    if (step <= this.currentStep || formValid) {
+      this.currentStep = step;
+    } else {
+      this.getCurrentStepGroup()?.markAllAsTouched();
+    }
+  }
+
+  getCurrentStepGroup(): FormGroup | null {
+    switch (this.currentStep) {
+      case 0: return this.desaparicionForm.get('personaDTO') as FormGroup;
+      case 1: return this.desaparicionForm.get('lugarDTO') as FormGroup;
+      default: return null;
+    }
+  }
+
 
   onSubmit(): void {
     if (this.desaparicionForm.invalid) {
