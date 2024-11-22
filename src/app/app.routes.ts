ac5c770830ae36046d848174075239b5c1a1e117
rafdiecas <rdiezcastro@safareyes.es>
2024-11-22 19:06:32 +0100

    mod - funcion para traer rol en authservice add - guards para los 3 roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,12 @@ import {LoginComponent} from './core/login/login.component';
 import {RegistraUsuarioComponent} from './core/registra-usuario/registra-usuario.component';
 import {AdminPanelComponent} from './features/admin-panel/admin-panel.component';
 import {AutoridadAvisosComponent} from './features/perfil-autoridad/componentes/autoridad-avisos/autoridad-avisos.component';
+import {civilGuard} from './core/guard/civil.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login',component: LoginComponent },
-  { path: 'usuario/civil', component:PerfilCivilComponent },
+  { path: 'usuario/civil', component:PerfilCivilComponent,  canActivate: [civilGuard] },
   { path: 'desaparicion/:id', component: DesaparicionComponent },
   { path: 'admin', component: AdminPanelComponent },
   { path: 'registro', component: RegistraUsuarioComponent },
