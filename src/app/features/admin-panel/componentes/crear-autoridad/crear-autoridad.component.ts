85781dfa2808f4edffb91b36c6dadc3fbc6ac1c6
Alex <amartinenghivelazquez@safareyes.es>
2024-12-10 17:48:15 +0100

    ultimos cambios maquetacion/responsive mobile

--- src/app/features/admin-panel/componentes/crear-autoridad/crear-autoridad.component.ts
+++ src/app/features/admin-panel/componentes/crear-autoridad/crear-autoridad.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {CommonModule} from '@angular/common';
-import {AutoridadService} from '../../../../servicios/autoridad.service';
-import {MatTooltip} from '@angular/material/tooltip';
+import {Component, inject, OnInit} from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormBuilder,
 
+  FormGroup,
+  ReactiveFormsModule, ValidationErrors,
+  Validators
+} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {AutoridadService} from
+    '../../../../servicios/autoridad.service';
+import {MatTooltip} from '@angular/material/tooltip';
+import {MatSnackBar, MatSnackBarModule} from
+    '@angular/material/snack-bar';
+import {Observable, of} from 'rxjs';
 /**
- * Componente que gestiona la creación de una autoridad desde el panel de administración.
+ * Componente que gestiona la creación de una autoridad desde el
+ panel de administración.
  */
 @Component({
   selector: 'app-crear-autoridad',
@@ -14,57 +26,90 @@ import {MatTooltip} from '@angular/material/tooltip';
     ReactiveFormsModule,
     CommonModule,
     MatTooltip,
+    MatSnackBarModule
   ],
   templateUrl: './crear-autoridad.component.html',
   styleUrls: ['./crear-autoridad.component.css'],
 })
 export class CrearAutoridadComponent implements OnInit {
   autoridadForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private autoridadService: AutoridadService) {
-    this.autoridadForm = this.fb.group({}); // Inicialización segura
+  private snackBar = inject(MatSnackBar);
+  constructor(private fb: FormBuilder, private autoridadService:
+  AutoridadService) {
+    this.autoridadForm = this.fb.group({});
   }
-
   ngOnInit(): void {
     this.autoridadForm = this.fb.group({
       identificador: ['', Validators.required],
       usuarioCrearDTO: this.fb.group(
         {
-          usuario: ['', Validators.required],
-          email: ['', [Validators.required, Validators.email]],
-          contrasenya: ['', [Validators.required, Validators.minLength(6)]],
+          usuario: [
+            '',
+            [Validators.required], // Validador sincrónico requerido
+            this.validarIdentificador() // Validador asincrónico
+          ],
+          email: ['', [Validators.required], this.validarCorreo()],
+
+          contrasenya: ['', [Validators.required,
+
+            Validators.minLength(6)]],
+
           repContrasenya: ['', Validators.required],
         },
-        { validators: this.matchPasswords('contrasenya', 'repContrasenya') }
+        { validators: this.matchPasswords('contrasenya',
+            'repContrasenya') }
       ),
     });
   }
 
   /**
-   * Comprueba que dos campos coincidan, en este caso, contrasenya y repContrasenya.
+   * Comprueba que dos campos coincidan, en este caso, contrasenya y
+   repContrasenya.
    * @param controlName
    * @param matchingControlName
    */
-  matchPasswords(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup): { [key: string]: boolean } | null => {
+  matchPasswords(controlName: string, matchingControlName: string)
+  {
+    return (formGroup: FormGroup): { [key: string]: boolean } |
+      null => {
       const control = formGroup.get(controlName);
       const matchingControl = formGroup.get(matchingControlName);
-
       if (!control || !matchingControl) {
         return null; // Si los controles no existen, no hay error
       }
-
-      if (matchingControl.errors && !matchingControl.errors['mustMatch']) {
+      if (matchingControl.errors &&
+        !matchingControl.errors['mustMatch']) {
         return null; // Si ya hay otros errores, no interferir
       }
-
       if (control.value !== matchingControl.value) {
         matchingControl.setErrors({ mustMatch: true });
       } else {
         matchingControl.setErrors(null);
       }
-
       return null; // Devuelve null si no hay errores
+    };
+  }
+  /**
+   * Valida que el identificador de la autoridad sea uno de los
+   permitidos.
+   */
+
+  validarIdentificador(): AsyncValidatorFn {
+    const patronesPermitidos = /^(CNP|GCV|BMB)/;
+    return (control: AbstractControl): Observable<ValidationErrors
+      | null> => {
+      const valor = control.value || '';
+      return of(patronesPermitidos.test(valor) ? null : {
+        identificadorInvalido: true });
+    };
+  }
+  validarCorreo(): AsyncValidatorFn {
+    const emailPermitidos = /^(.*@(cnp\.es|gcv\.es|bomb\.es))$/;
+    return (control: AbstractControl): Observable<ValidationErrors
+      | null> => {
+      const valor = control.value || '';
+      return of(emailPermitidos.test(valor) ? null : {
+        emailInvalido: true });
     };
   }
 
@@ -74,21 +119,27 @@ export class CrearAutoridadComponent implements OnInit {
   onSubmit(): void {
     if (this.autoridadForm.valid) {
       const autoridadData = this.autoridadForm.value;
+      const loadingSnackbar = this.snackBar.open('Creando autoridad...', 'Cerrar');
 
-      // Llama al servicio para enviar los datos al backend
+// Llama al servicio para enviar los datos al backend
       this.autoridadService.crearAutoridad(autoridadData).subscribe({
         next: (response) => {
+          loadingSnackbar.dismiss();
+          this.snackBar.open('Autoridad creada con éxito', 'Cerrar', {
+            duration: 2000,
+            });
           console.log('Respuesta del servidor:', response);
-          this.autoridadForm.reset(); // Limpia el formulario tras el envío exitoso
+          this.autoridadForm.reset();
+
         },
         error: (error) => {
           console.error('Error al enviar los datos:', error);
+
         },
       });
     } else {
       this.autoridadForm.markAllAsTouched();
     }
   }
-
 
 }
